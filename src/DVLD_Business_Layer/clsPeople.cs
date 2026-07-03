7a3716a515a57afcfbffa0ee765b4a6f36eccee9
Abdalrahman Walid <alramly1451@gmail.com>
2026-07-03 11:37:41 +0300

    set data field for person

--- src/DVLD_Business_Layer/clsPeople.cs
+++ src/DVLD_Business_Layer/clsPeople.cs
@@ -8,5 +8,21 @@ namespace DVLD_Business_Layer
 {
     public class clsPeople
     {
+        enum enMode { Add, Update }
+        public int ID { get; set; }
+        public int NationalNo { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Gendor { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int NationalityCountryID { get; set; }
+        public string ImagePath { get; set; }
+        enMode Mode { get; set; }
+
     }
 }
