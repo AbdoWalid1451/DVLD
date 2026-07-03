ec8031f983a467c2abfd3d6e020fab3900429a36
Abdalrahman Walid <alramly1451@gmail.com>
2026-07-03 11:38:54 +0300

    rename to person

--- src/DVLD_Business_Layer/clsPeople.cs
+++ src/DVLD_Business_Layer/clsPeople.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DVLD_Business_Layer
 {
-    public class clsPeople
+    public class clsPerson
     {
         enum enMode { Add, Update }
         public int ID { get; set; }
